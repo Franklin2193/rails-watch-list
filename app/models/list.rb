d5d912bce8fa87359f2fa66e376bab412fa9b5c7
franklin duque <franklinmont@gmail.com>
2022-09-17 15:07:45 -0500

    conitnuacion

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
-  #has_many :movies
-  has_many :movies, through: :bookmarks
+  # has_many :bookmarks
   has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  # has_many :bookmarks, dependent: :destroy
   validates :name, uniqueness: true, presence: true
 end
